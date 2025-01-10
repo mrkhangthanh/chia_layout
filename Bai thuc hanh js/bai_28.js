fd97891b06429780235469b529d2db74d49e8485
mrkhangthanh <mrthanhgiangkhang@gmail.com>
2025-01-10 20:00:01 +0700

    sad
    sadf

--- Bai thuc hanh js/bai_28.js
+++ Bai thuc hanh js/bai_28.js
@@ -1,12 +1,18 @@
 // 28)Hãy tính tích các chữ số lẻ của số nguyên dương n
 
-let n = 3;
-let tich_le = 1;
+// let n = 3;
+// let tich_le = 1;
 
-for( let i =  0; i <= n; i++){
-    if(i % 2 !== 0){
-        tich_le *= i;
+// for( let i =  0; i <= n; i++){
+//     if(i % 2 !== 0){
+//         tich_le *= i;
 
-    }
-}
-console.log(tich_le);
+//     }
+// }
+// console.log(tich_le);
+ketQua = 0;
+  for (var n = 0; n <= 100; n++){
+    ketQua += n;
+    // console.log(ketQua);
+  }
+  console.log(ketQua);
